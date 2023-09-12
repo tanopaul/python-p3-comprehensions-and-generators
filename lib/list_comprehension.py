2b77bee3c595d1d273b07939ae78a4bb9310014b
Tano Paul <tanopaul38@gmail.com>
2023-09-12 14:33:44 -0400

    completed comprehention generator in python

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [n for n in num_list if n % 2 == 0]
+    print(even_numbers)
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    exclamations = [n + '!' for n in sentence_list]
+    print(exclamations)
+    return exclamations
+
